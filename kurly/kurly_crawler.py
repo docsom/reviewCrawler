b1568419804d70e362a76b56f34ecda550fb8bd0
Jongmin Ahn <ajm6962@hanmail.net>
2022-09-15 17:12:56 +0900

    extract all information in 1 page, 7 review

--- kurly/kurly_crawler.py
+++ kurly/kurly_crawler.py
@@ -22,14 +22,20 @@ if response.status_code == 200:
     for review_html in reviews_html:
         table_html = review_html.select('table > tr > td')
         
-        review_num = table_html[0].get_text().replace("\n", "")
-        review_title = table_html[1].select_one('div').get_text().replace("\n", " ")
-        review_user_grade = table_html[2].get_text().replace("\n", "")
-        review_user_name = table_html[3].get_text().replace("\n", "")
-        review_time = table_html[4].get_text().replace("\n", "")
-        review_like_cnt = table_html[5].get_text().replace("\n", "")
+        review_num = table_html[0].get_text().replace("\n", "").strip()
+        review_title = table_html[1].select_one('div').get_text().replace("\n", " ").strip()
+        review_user_grade = table_html[2].get_text().replace("\n", "").strip()
+        review_user_name = table_html[3].get_text().replace("\n", "").strip()
+        review_time = table_html[4].get_text().replace("\n", "").strip()
+        review_like_cnt = table_html[5].get_text().replace("\n", "").strip()
         
-        print( "#", review_num, "#", review_title,  "#", review_user_grade,  "#", review_user_name,  "#", review_time,  "#", review_like_cnt, "#")
+        
+        inner_review_html = review_html.select_one("div.review_view > div.inner_review")
+        product_name = inner_review_html.select_one("div.name_purchase > strong").get_text().replace("\n", "")
+        review_text = inner_review_html.get_text().replace("\n", " ").replace(product_name, "").strip()
+        
+        
+        #print( "#", review_num, "#", review_title,  "#", review_user_grade,  "#", review_user_name,  "#", review_time,  "#", review_like_cnt, "#")
         break
 else:
     print(response.status.code)
