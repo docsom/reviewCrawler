cd0b455b1b7b0c5f1c2e35912c1bd859047a1cf7
Jongmin Ahn <ajm6962@hanmail.net>
2022-09-16 15:59:56 +0900

    add saving function in product crawler

--- kurly/product_crawler.py
+++ kurly/product_crawler.py
@@ -1,5 +1,32 @@
 import requests
 import json
+import os
+from csv import DictWriter
+
+headersCSV = [
+    'no',
+    'name',
+    'short_description',
+    'list_image_url',
+    'sales_price',
+    'discounted_price',
+    'discount_rate',
+    'is_buy_now',
+    'is_purchase_status',
+    'is_giftable',
+    'is_only_adult',
+    'is_sold_out',
+    'sold_out_title',
+    'sold_out_text',
+    'can_restock_notify',
+    'tags',
+    'sticker',
+    'is_multiple_price',
+    'group_product',
+    'product_view_status',
+    'not_purchase_message',
+    'delivery_type_names',    
+]
 
 def get_auth_token():
     token_url = "https://www.kurly.com/nx/api/session"
@@ -8,7 +35,7 @@ def get_auth_token():
     return json.loads(token_response.text)['accessToken']
 
 
-def collect_product_info_in_single_category(_category_id, _sort_type=4):
+def get_products_info_in_single_category(_category_id, _sort_type=4):
     '''
     return type: list[json]
     '''
@@ -19,6 +46,7 @@ def collect_product_info_in_single_category(_category_id, _sort_type=4):
 
     isNext = True
     page = 1
+    product_id_list = []
     product_list = []
 
     while isNext == True:
@@ -34,6 +62,7 @@ def collect_product_info_in_single_category(_category_id, _sort_type=4):
             
             for product in data:
                 product_list.append(product)
+                product_id_list.append(product['no'])
         
             if response_json['meta']['pagination']['current_page'] == response_json['meta']['pagination']['total_pages']:
                 print("All of products in category:{} is taken.".format(_category_id))
@@ -45,6 +74,36 @@ def collect_product_info_in_single_category(_category_id, _sort_type=4):
             print(response.status.code)
             print("There is an error while making list of products")
     
-    return product_list
+    return product_list, product_id_list
 
-print(collect_product_info_in_single_category(913008))
+
+def get_save_products_info_in_single_category(_category_id, _sort_type=4):
+    
+    nowLoc = os.getcwd()
+    dirLoc = "{}/data/kurly/cat_{}".format(nowLoc, _category_id)
+
+    try:
+        dirExist = os.path.exists(dirLoc)
+        if not dirExist :
+            os.makedirs(dirLoc)
+    except OSError:
+            print("Error: Creating Dir {}".format(dirLoc))
+
+    product_list, product_id_list = get_products_info_in_single_category(_category_id, _sort_type)
+
+    proListLoc = "{}/info_{}".format(dirLoc, _category_id)
+    proIdListLoc = "{}/ids_{}".format(dirLoc, _category_id)
+
+    with open('{}.txt'.format(proIdListLoc), 'w', encoding='utf-8-sig') as f_object:
+        for id in product_id_list:
+            f_object.write(str(id)+'\n')
+
+    with open('{}.csv'.format(proListLoc), 'a', newline='', encoding='utf-8-sig') as f_object:
+        dictwriter_object = DictWriter(f_object, fieldnames=headersCSV)
+        dictwriter_object.writeheader()
+        for i in product_list:
+            dictwriter_object.writerow(i)
+            
+
+_category_id = 913008
+get_save_products_info_in_single_category(_category_id)
