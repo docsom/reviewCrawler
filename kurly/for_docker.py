00bdea36254ed965e70df2dce165788aa11c9dd4
Jongmin Ahn <ajm6962@hanmail.net>
2022-09-21 09:33:14 +0900

    edit for_docker in kurly

--- kurly/for_docker.py
+++ kurly/for_docker.py
@@ -3,4 +3,12 @@ from review_crawler import get_save_reviews_in_single_category
 from product_crawler import get_save_product_info_in_all_category
 
 
-get_save_reviews_in_single_category(911006)
+
+from review_manager import ReviewManager
+
+category_id = 911006
+
+reviewManager = ReviewManager(category_id)
+reviewManager.info()
+
+get_save_reviews_in_single_category(category_id)
