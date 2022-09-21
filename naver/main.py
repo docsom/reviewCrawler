b8acf9a562bca0fc40da1ed58df8150787693360
Taeseung <kts0921@unist.ac.kr>
2022-09-21 17:44:05 +0900

    0921 naver commit

--- naver/main.py
+++ naver/main.py
@@ -1,25 +1,29 @@
 import requests
+from bs4 import BeautifulSoup
+import json
+from csv import DictWriter
 
-cookies = {
-    'NID_AUT': 'hlcCjDBBfth27IPEsMK+7OWdkxWKATa236aORiMrpQi/aQGFSlS2F4kQvtAJx/am',
-    'NID_JKL': '7Dh1C3ZrvIbFdsM9f955aS+yDhIM9dadsuhbEFsABms=',
-    'NaverSuggestUse': 'use%26unuse',
-    'nx_ssl': '2',
-    'BMR': 's=1663135968254&r=https%3A%2F%2Fm.blog.naver.com%2Fgassi00%2F222302178273&r2=https%3A%2F%2Fwww.google.com%2F',
-    'page_uid': 'hykGLwp0J1ZssgplRTGssssssHN-517530',
-    'NID_SES': 'AAABn5rt1BakJNviHH3yBQ/HH4zzYV40HmtZAkkmbgZh2Fi8tPFEkbTgSAx3viGpS42BTX8GM39WD/u8/AlcPQFzKpVGHiSGi8gStC0TrLderyhTVAsPMtE8/4b+addtpy7Rfgf/hHG2rVtMlK8FOPKOLyKOQiRL2/Qlyrbhj0cCgBA+A+EcFAvhBiCPGKEHUmU4Ej8Gat5/PznhERFqvFwOH2Rk+N2tWtNJB/KzI8jDZD7KL/amC+x+hTBVVeNcTgvvZfU4wsnGdxEvO4p4xCshc4VN0S4WY98k/i32GfJFxAqqC/b8CBY3eGZ2ST4r5GM/M2cCXlAYyARwVRNl2S/RlClKgv3U4Lm4QEMV7DZ8I+FTt7coRHHBJTirpju2Ye0f0F/VEzoPREhLiDo+AMGIA8WIvvzmuCUJQoeGWDP5/SGSswQrwqHBzZWx51UFP41y/CyPpVIRxkmg4fGWfjFKKZj4IdUBI3dfb3TXXeGE6rvlp8dE3tYEsY68v5GBNTvmACn/idPqn8O3QWuOif8FVMf54/OajJyGogFX1wPvP/NZ',
-    'SBC': '0b2b33af-24a0-4c76-a75f-c9d8a6686a76',
-}
+headersCSV = [
+    "reviewContent",
+]
+
+target_url = 'https://smartstore.naver.com/cakefactoryd/products/6960829129'#'https://smartstore.naver.com/heefoodstory/products/3955378450'
+
+html = requests.get(target_url).text
+soup = BeautifulSoup(html,'html.parser')
+dict = soup.select_one('body > script:nth-child(2)').get_text()
+dict = dict[27:]
+json_object = json.loads(dict)
+merchant_num = json_object['smartStoreV2']['channel']['payReferenceKey']
+product_num = json_object['product']['A']['productNo']
 
 headers = {
     'authority': 'smartstore.naver.com',
     'accept': 'application/json, text/plain, */*',
-    'accept-language': 'ko,en;q=0.9,en-US;q=0.8',
+    'accept-language': 'ko',
     'content-type': 'application/json;charset=UTF-8',
-    # Requests sorts cookies= alphabetically
-    # 'cookie': 'NID_AUT=hlcCjDBBfth27IPEsMK+7OWdkxWKATa236aORiMrpQi/aQGFSlS2F4kQvtAJx/am; NID_JKL=7Dh1C3ZrvIbFdsM9f955aS+yDhIM9dadsuhbEFsABms=; NaverSuggestUse=use%26unuse; nx_ssl=2; BMR=s=1663135968254&r=https%3A%2F%2Fm.blog.naver.com%2Fgassi00%2F222302178273&r2=https%3A%2F%2Fwww.google.com%2F; page_uid=hykGLwp0J1ZssgplRTGssssssHN-517530; NID_SES=AAABn5rt1BakJNviHH3yBQ/HH4zzYV40HmtZAkkmbgZh2Fi8tPFEkbTgSAx3viGpS42BTX8GM39WD/u8/AlcPQFzKpVGHiSGi8gStC0TrLderyhTVAsPMtE8/4b+addtpy7Rfgf/hHG2rVtMlK8FOPKOLyKOQiRL2/Qlyrbhj0cCgBA+A+EcFAvhBiCPGKEHUmU4Ej8Gat5/PznhERFqvFwOH2Rk+N2tWtNJB/KzI8jDZD7KL/amC+x+hTBVVeNcTgvvZfU4wsnGdxEvO4p4xCshc4VN0S4WY98k/i32GfJFxAqqC/b8CBY3eGZ2ST4r5GM/M2cCXlAYyARwVRNl2S/RlClKgv3U4Lm4QEMV7DZ8I+FTt7coRHHBJTirpju2Ye0f0F/VEzoPREhLiDo+AMGIA8WIvvzmuCUJQoeGWDP5/SGSswQrwqHBzZWx51UFP41y/CyPpVIRxkmg4fGWfjFKKZj4IdUBI3dfb3TXXeGE6rvlp8dE3tYEsY68v5GBNTvmACn/idPqn8O3QWuOif8FVMf54/OajJyGogFX1wPvP/NZ; SBC=0b2b33af-24a0-4c76-a75f-c9d8a6686a76',
     'origin': 'https://smartstore.naver.com',
-    'referer': 'https://smartstore.naver.com/raintato/products/5089990614',
+    'referer': target_url,
     'sec-ch-ua': '"Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
@@ -32,13 +36,24 @@ headers = {
 json_data = {
     'page': 1,
     'pageSize': 20,
-    'merchantNo': '500158087',
-    'originProductNo': '5071615198',
+    'merchantNo': merchant_num,
+    'originProductNo': product_num,
     'sortType': 'REVIEW_RANKING',
 }
 
-response = requests.post('https://smartstore.naver.com/i/v1/reviews/paged-reviews', cookies=cookies, headers=headers, json=json_data)
-print(response.json())
-import pprint
+response = requests.post('https://smartstore.naver.com/i/v1/reviews/paged-reviews', headers=headers, json=json_data)
+data = response.json()
+totalPages = data['totalPages']
 
-# pprint.pprint(response.text)
+for i in range(totalPages): # 전체 페이지 수 만큼 반복
+    print(i)
+    with open('{}.csv'.format('test'), 'a', newline='', encoding="utf-8-sig") as f_object:
+        dictwriter_object = DictWriter(f_object, fieldnames=headersCSV)
+        dictwriter_object.writeheader()
+        for i in range(20): # 마지막 루프때 게시글 수 디버깅하기
+            review = data['contents'][i]["reviewContent"]
+            dict = {'reviewContent':review}
+            dictwriter_object.writerow(dict)
+    json_data['page'] += 1 # 다음 페이지 넘어가기
+    response = requests.post('https://smartstore.naver.com/i/v1/reviews/paged-reviews', headers=headers, json=json_data)
+    data = response.json()
