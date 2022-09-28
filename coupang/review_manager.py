47d2930c9535de4d11b34f58cc75061365c8391f
Jongmin Ahn <ajm6962@hanmail.net>
2022-09-28 15:36:16 +0900

    finish review_manager.py

--- coupang/review_manager.py
+++ coupang/review_manager.py
@@ -18,13 +18,6 @@ import pandas as pd
 
 now_loc = os.getcwd()
 coup_loc = '{}/data/coupang'.format(now_loc)
-#review_loc = '{}/reviews/{}.csv'.format(cat_loc, product_id)
-
-
-
-# categories = os.listdir(coup_loc)
-# categories.remove('log.csv')
-
     
 def extract_info_in_reviews_csv(category_id, product_id):
     product_loc = '{}/{}/reviews/{}.csv'.format(coup_loc, category_id, product_id)
@@ -40,7 +33,7 @@ def extract_info_in_reviews_csv(category_id, product_id):
         info_dict = {
             'category_id' : category_id,
             'product_id' : product_id,
-            'total_num' : num_total_reviews,
+            'total_review_num' : num_total_reviews,
             'duplicated_num' : orig_num_reviews - num_total_reviews,
             'num_by_rating' : num_by_rating
         }
@@ -50,7 +43,7 @@ def extract_info_in_reviews_csv(category_id, product_id):
         info_dict = {
             'category_id' : category_id,
             'product_id' : product_id,
-            'total_num' : 0,
+            'total_review_num' : 0,
             'duplicated_num' : 0,
             'num_by_rating' : [0, 0, 0, 0, 0, 0]
         }
@@ -59,9 +52,8 @@ def extract_info_in_reviews_csv(category_id, product_id):
 
 def extract_info_in_single_category(category_id):
     std_products_loc = '{}/{}/{}.csv'.format(coup_loc, category_id, category_id)
-    
-    if os.path.isdir(std_products_loc) == False:
-        print('Something is wrong. How did you make this folder?')
+    if os.path.isfile(std_products_loc) == False:
+        print('No Product_List of Single Category: {}'.format(category_id))
         raise FileNotFoundError
     
     std_products = pd.read_csv(std_products_loc).product_id.drop_duplicates().reset_index(drop=True)
@@ -69,12 +61,12 @@ def extract_info_in_single_category(category_id):
     if os.path.isdir(now_reviews_loc):
         now_products =[id.rstrip('.csv') for id in os.listdir(now_reviews_loc)]
         ratings_num = [0, 0, 0, 0, 0, 0]
-        total_num = 0
+        total_review_num = 0
         each_product_info = []
         for product_id in now_products:
             product_info = extract_info_in_reviews_csv(category_id, product_id)
             each_product_info.append(product_info)
-            total_num += product_info['total_num']
+            total_review_num += product_info['total_review_num']
             for i in range(1, 6):
                 ratings_num[i] += product_info['num_by_rating'][i]
             
@@ -82,7 +74,7 @@ def extract_info_in_single_category(category_id):
             'category_id' : category_id,
             'target_num_of_products' : len(std_products),
             'got_num_of_products' : len(now_products),
-            'total_num' : total_num,
+            'total_review_num' : total_review_num,
             'num_by_rating' : ratings_num,
             'each_product_info' : each_product_info
         }
@@ -93,7 +85,7 @@ def extract_info_in_single_category(category_id):
             'category_id' : category_id,
             'target_num_of_products' : len(std_products),
             'got_num_of_products' : 0,
-            'total_num' : 0,
+            'total_review_num' : 0,
             'num_by_rating' : [0,0,0,0,0,0],
             'each_product_info' : []
         }
@@ -102,6 +94,28 @@ def extract_info_in_single_category(category_id):
 def extract_info_in_all_category():
     categories = os.listdir(coup_loc)
     categories.remove('log.csv')
+    ratings_num = [0, 0, 0, 0, 0, 0]
+    target_num_of_products = 0
+    got_num_of_products = 0
+    total_review_num = 0
+    each_category_info = []
     for category_id in categories:
         category_info = extract_info_in_single_category(category_id)
+        target_num_of_products += category_info['target_num_of_products']
+        got_num_of_products += category_info['got_num_of_products']
+        total_review_num += category_info['total_review_num']
+        for i in range(1, 6):
+            ratings_num[i] += category_info['num_by_rating'][i]
         
+        each_category_info.append(category_info)
+    
+    info_dict = {
+        'target_num_of_products': target_num_of_products,
+        'got_num_of_products': got_num_of_products,
+        'total_review_num' : total_review_num,
+        'num_by_rating' : ratings_num,
+        'each_category_info' : each_category_info
+    }
+    return info_dict
+
+print(extract_info_in_all_category())
