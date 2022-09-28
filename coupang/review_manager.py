e8659baba61ae52de389f34fd5e87e93c3e169d0
Jongmin Ahn <ajm6962@hanmail.net>
2022-09-28 15:14:31 +0900

    make managers

--- coupang/review_manager.py
+++ coupang/review_manager.py
@@ -12,3 +12,96 @@
 # log에서 error가 떴을 경우에 해당 카테고리를 지우는 프로그램도 좀 만들어봐 이거 하나하나 지우기 힘드니까
 
 # 도커에서 안돌아가는거 방안 좀 생각해봐
+# 마켓컬리 데이터 집어넣고 컨플에 정리해
+import os
+import pandas as pd
+
+now_loc = os.getcwd()
+coup_loc = '{}/data/coupang'.format(now_loc)
+#review_loc = '{}/reviews/{}.csv'.format(cat_loc, product_id)
+
+
+
+# categories = os.listdir(coup_loc)
+# categories.remove('log.csv')
+
+    
+def extract_info_in_reviews_csv(category_id, product_id):
+    product_loc = '{}/{}/reviews/{}.csv'.format(coup_loc, category_id, product_id)
+    try:
+        reviews = pd.read_csv(product_loc)
+        orig_num_reviews = len(reviews)
+        reviews.drop_duplicates('review_id', inplace = True)
+        num_total_reviews = len(reviews)
+        num_by_rating = [0]
+        for i in range(1, 6):
+            num_by_rating.append(len(reviews.loc[reviews.review_user_grade == i]))
+
+        info_dict = {
+            'category_id' : category_id,
+            'product_id' : product_id,
+            'total_num' : num_total_reviews,
+            'duplicated_num' : orig_num_reviews - num_total_reviews,
+            'num_by_rating' : num_by_rating
+        }
+        return info_dict
+    
+    except FileNotFoundError:
+        info_dict = {
+            'category_id' : category_id,
+            'product_id' : product_id,
+            'total_num' : 0,
+            'duplicated_num' : 0,
+            'num_by_rating' : [0, 0, 0, 0, 0, 0]
+        }
+        return info_dict
+        
+
+def extract_info_in_single_category(category_id):
+    std_products_loc = '{}/{}/{}.csv'.format(coup_loc, category_id, category_id)
+    
+    if os.path.isdir(std_products_loc) == False:
+        print('Something is wrong. How did you make this folder?')
+        raise FileNotFoundError
+    
+    std_products = pd.read_csv(std_products_loc).product_id.drop_duplicates().reset_index(drop=True)
+    now_reviews_loc = '{}/{}/reviews'.format(coup_loc, category_id)
+    if os.path.isdir(now_reviews_loc):
+        now_products =[id.rstrip('.csv') for id in os.listdir(now_reviews_loc)]
+        ratings_num = [0, 0, 0, 0, 0, 0]
+        total_num = 0
+        each_product_info = []
+        for product_id in now_products:
+            product_info = extract_info_in_reviews_csv(category_id, product_id)
+            each_product_info.append(product_info)
+            total_num += product_info['total_num']
+            for i in range(1, 6):
+                ratings_num[i] += product_info['num_by_rating'][i]
+            
+        info_dict = {
+            'category_id' : category_id,
+            'target_num_of_products' : len(std_products),
+            'got_num_of_products' : len(now_products),
+            'total_num' : total_num,
+            'num_by_rating' : ratings_num,
+            'each_product_info' : each_product_info
+        }
+        return info_dict
+    
+    else:
+        info_dict = {
+            'category_id' : category_id,
+            'target_num_of_products' : len(std_products),
+            'got_num_of_products' : 0,
+            'total_num' : 0,
+            'num_by_rating' : [0,0,0,0,0,0],
+            'each_product_info' : []
+        }
+        return info_dict
+        
+def extract_info_in_all_category():
+    categories = os.listdir(coup_loc)
+    categories.remove('log.csv')
+    for category_id in categories:
+        category_info = extract_info_in_single_category(category_id)
+        
