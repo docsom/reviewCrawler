ce8134b441d6bcd631e0be904ee0b389da703fbb
Taeseung <kts0921@unist.ac.kr>
2022-09-30 12:03:28 +0900

    0930 naver commit

--- naver/main.py
+++ naver/main.py
@@ -13,16 +13,13 @@ with open("data/naver/ids_100002454.txt", "r", encoding='utf-8-sig') as f:
     URLs = f.read().splitlines()
     products = [url[43:] for url in URLs]
 
-print(URLs)
-print(products)
-
-# if __name__ == '__main__':
-#     start_time = time.time()
-#     pool = multiprocessing.Pool(processes=8)
-#     func = partial(reviewCrawler, category_id= 100002454, sortTypeNum=3)
-#     pool.map(func, URLs, products)
-#     pool.close()
-#     pool.join()
-#     print("--- elapsed time %s seconds ---" % (time.time() - start_time))
+if __name__ == '__main__':
+    start_time = time.time()
+    pool = multiprocessing.Pool(processes=8)
+    func = partial(reviewCrawler, category_id= 100002454, sortTypeNum=3)
+    pool.starmap(func, URLs, products)
+    pool.close()
+    pool.join()
+    print("--- elapsed time %s seconds ---" % (time.time() - start_time))
 
 # 데이터 폴더에 카테고리 있는지 확인하고 폴더 만들기
